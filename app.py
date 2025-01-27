47c4bf79a456e35e63a79f4b0731876b1955e5b3
Hien Bui <u7809977@anu.edu.au>
2025-01-27 17:34:49 +1100

    test reducing data with vercel

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import pickle
 import random as rd
 
-from bot_responser import (
-    vector_embedder, matrices, generator, doc_id_comments_mapper,
-    find_response_message, paraphrase_message
-)
+# from bot_responser import (
+#     vector_embedder, matrices, generator, doc_id_comments_mapper,
+#     find_response_message, paraphrase_message
+# )
 
 from io import BytesIO
 import gdown
@@ -87,15 +87,15 @@ def greeting():
 def chat():
     user_message = request.json.get('message')
 
-    response_comments = find_response_message(user_message, matrices, doc_id_comments_mapper, vector_embedder)
-    response = paraphrase_message(response_comments, generator)
-
-    class_proba = classifier(user_message, vector_embedder, classifier_model)
-    classifier_text = prepare_classifier_message(class_proba, classifier_model.classes_)
-    response = f"{classifier_text}\n\n{response}"
-
-    return jsonify({'reply': response})
-    # return jsonify({'reply': user_message})
+    # response_comments = find_response_message(user_message, matrices, doc_id_comments_mapper, vector_embedder)
+    # response = paraphrase_message(response_comments, generator)
+    #
+    # class_proba = classifier(user_message, vector_embedder, classifier_model)
+    # classifier_text = prepare_classifier_message(class_proba, classifier_model.classes_)
+    # response = f"{classifier_text}\n\n{response}"
+    #
+    # return jsonify({'reply': response})
+    return jsonify({'reply': user_message})
 
 
 if __name__ == '__main__':
